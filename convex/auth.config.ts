ba2cba472556884c85f45148ecde6c58fbb7bc61
amriteshraj98 <amritesh1798@gmail.com>
2025-07-16 18:52:46 +0530

    authentication ,database and stream setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://present-muskox-61.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="m-10 flex h-screen items-center justify-center">
+        <SignedOut>
+              <SignInButton />
+              
+        </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+      
+     
+    </div>
+
+  );
+}
